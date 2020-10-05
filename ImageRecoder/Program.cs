fdb2ce584c006c8b1083a89b323e929debb66c17
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-10-05 17:00:56 +0200

    Apply some C# conventions, add seed to Random instance and some other minor fixes

--- ImageRecoder/Program.cs
+++ ImageRecoder/Program.cs
@@ -2,16 +2,16 @@
 using CommandLine.Text;
 using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
-using System.Text;
 
 namespace ImageRecoder
 {
-    class Program
+    public static class Program
     {
-        public static Random rng = new Random();
+        private static readonly Random Rng = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var parser = new Parser(with => with.HelpWriter = null);
             var result = parser.ParseArguments<Options>(args);
@@ -71,25 +71,24 @@ namespace ImageRecoder
         /// <param name="inp">Bitmap to apply a resize to</param>
         private static void ApplyRandomResize(ref Bitmap inp)
         {
-            int NewW, NewH;
+            double wPerc = (double)Rng.Next(-20, 20) / 100;
+            double hPerc = (double)Rng.Next(-20, 20) / 100;
 
-            double WPerc = (double)rng.Next(-20, 20) / 100; double HPerc = (double)rng.Next(-20, 20) / 100;
+            int newW = (int)Math.Round(inp.Width + inp.Width * wPerc);
+            int newH = (int)Math.Round(inp.Height + inp.Height * hPerc);
 
-            NewW = (int)Math.Round(inp.Width + inp.Width * WPerc); NewH = (int)Math.Round(inp.Height + inp.Height * HPerc);
-
-            Bitmap bmp = new Bitmap(NewW, NewH);
+            using Bitmap bmp = new Bitmap(newW, newH);
             Graphics g = Graphics.FromImage(bmp);
 
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-            g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
-            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
-            g.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.CompositingMode = CompositingMode.SourceCopy;
 
-            g.DrawImage(inp, new Rectangle(Point.Empty, new Size(NewW, NewH)));
+            g.DrawImage(inp, new Rectangle(Point.Empty, new Size(newW, newH)));
 
             inp = new Bitmap(bmp);
-            bmp.Dispose();
         }
 
         /// <summary>
@@ -98,7 +97,7 @@ namespace ImageRecoder
         /// <param name="bmp">Bitmap to copy pixels from</param>
         /// <param name="meta">Meta to apply</param>
         /// <returns>New bitmap with meta applied to every pixel.</returns>
-        public static Bitmap SimpleRecode(Bitmap bmp, Meta meta)
+        private static Bitmap SimpleRecode(Bitmap bmp, Meta meta)
         {
             var bitmap = new Bitmap(bmp.Width, bmp.Height);
             for (int y = 0; y < bmp.Height; y++)
@@ -119,22 +118,22 @@ namespace ImageRecoder
         /// <param name="bmp">Bitmap to convert to raw BMP image</param>
         /// <param name="meta">Meta to apply to every pixel</param>
         /// <returns>Byte array representation of a BMP image</returns>
-        public static byte[] BMPRecode(Bitmap bmp, Meta meta)
+        private static byte[] BMPRecode(Bitmap bmp, Meta meta)
         {
             MemoryStream ms = new MemoryStream();
             Baker.BMHeader(ms);
-            int PixelAmount = bmp.Width * bmp.Height;
-            int BMPSize = PixelAmount * 3 + //1 pixel = 3 bytes.
+            int pixelAmount = bmp.Width * bmp.Height;
+            int bmpSize = pixelAmount * 3 + //1 pixel = 3 bytes.
                                 bmp.Height * (bmp.Width % 4) + //Total length of padding
                                 54; //54 bytes is the length of the whole header part.
-            Baker.FileSizeHeader(ms, BMPSize);
+            Baker.FileSizeHeader(ms, bmpSize);
             //Application specific header. It will be...
             Baker.IKTMHeader(ms, new byte[] { 0x00, 0x00, 0x00, 0x00 }); //There was an "IKTM" header but now just zeroes
-                                          //Static offset of pixel data.
+                                                                         //Static offset of pixel data.
             Baker.PixelOffsetHeader(ms);
             //DIB header, fully handled by one function (it's mostly static.)
             Baker.DIB(ms, bmp.Width, bmp.Height);
-            int Padding = bmp.Width % 4;
+            int padding = bmp.Width % 4;
             for (int y = bmp.Height - 1; y >= 0; y--)
             {
                 for (int x = 0; x < bmp.Width; x++)
@@ -143,7 +142,7 @@ namespace ImageRecoder
                     ApplyMeta(ref c, meta);
                     ms.Write(new byte[] { c.B, c.G, c.R });
                 }
-                ms.Write(new byte[Padding]);
+                ms.Write(new byte[padding]);
             }
             ms.Position = 0;
             byte[] o = new byte[ms.Length];
@@ -162,7 +161,7 @@ namespace ImageRecoder
 
             if (meta != Meta.BlackWhite)
             {
-                int Ratio = meta switch
+                int ratio = meta switch
                 {
                     Meta.OneFifth => 5,
                     Meta.OneTenth => 10,
@@ -172,109 +171,107 @@ namespace ImageRecoder
                     _ => throw new ApplicationException($"Meta value {meta} hasn't been implemented, please contact @kolya5544")
                 };
 
-                int R = c.R;
-                int G = c.G;
-                int B = c.B;
-                int NewR = (R / Ratio) * Ratio;
-                int NewG = (G / Ratio) * Ratio;
-                int NewB = (B / Ratio) * Ratio;
-                c = Color.FromArgb(c.A, NewR, NewG, NewB);
-            } else
+                int newR = (c.R / ratio) * ratio;
+                int newG = (c.G / ratio) * ratio;
+                int newB = (c.B / ratio) * ratio;
+                c = Color.FromArgb(c.A, newR, newG, newB);
+            }
+            else
             {
-                double Colour = c.R * 0x10000 + c.G * 0x100 + c.B;
-                double BWpercentage = Colour / 0xFFFFFF;
-                int BW = (int)Math.Round(BWpercentage * 255);
+                double colour = c.R * 0x10000 + c.G * 0x100 + c.B;
+                double bwPercentage = colour / 0xFFFFFF;
+                int bw = (int)Math.Round(bwPercentage * 255);
 
-                c = Color.FromArgb(BW, BW, BW);
+                c = Color.FromArgb(bw, bw, bw);
             }
         }
-    }
 
-    /// <summary>
-    /// A class to work with raw BMP
-    /// </summary>
-    public class Baker
-    {
-        private static byte[] IDField = { 0x42, 0x4D }; //"BM"
-        private static byte[] DataOffset = { 0x36, 0x00, 0x00, 0x00 }; //54.
-        private static byte[] DIBNumber = { 0x28, 0x00, 0x00, 0x00 }; //40.
-        private static byte[] DIB_MagicBytes_1 = {
+        /// <summary>
+        /// A class to work with raw BMP
+        /// </summary>
+        public static class Baker
+        {
+            private readonly static byte[] IDField = { 0x42, 0x4D }; //"BM"
+            private readonly static byte[] DataOffset = { 0x36, 0x00, 0x00, 0x00 }; //54.
+            private readonly static byte[] DIBNumber = { 0x28, 0x00, 0x00, 0x00 }; //40.
+            private readonly static byte[] DIB_MagicBytes_1 = {
             0x01, 0x00,//plane amount
             0x18, 0x00,//Bits per pixel.
             0x00, 0x00, 0x00, 0x00 //zero compression
         };
-        private static byte[] DIB_MagicBytes_2 =
-        {
+            private static readonly byte[] DIB_MagicBytes_2 =
+            {
             0x13, 0x0B, 0x00, 0x00, //Printing details =======
             0x13, 0x0B, 0x00, 0x00, //========================
             0x00, 0x00, 0x00, 0x00, //colors amount
             0x00, 0x00, 0x00, 0x00 // all colors are important
         };
 
-        /// <summary>
-        /// Adds "BM" header to the file.
-        /// </summary>
-        /// <param name="s"></param>
-        public static void BMHeader(Stream s)
-        {
-            s.Write(IDField);
-        }
-        /// <summary>
-        /// Adds BMP size header to the file.
-        /// </summary>
-        /// <param name="s"></param>
-        /// <param name="BMPSize"></param>
-        public static void FileSizeHeader(Stream s, int BMPSize)
-        {
-            byte[] Bytes = BitConverter.GetBytes(BMPSize);
-            s.Write(Bytes);
-        }
-        /// <summary>
-        /// Adds program-specific unused value to the file.
-        /// </summary>
-        /// <param name="s"></param>
-        /// <param name="header"></param>
-        public static void IKTMHeader(Stream s, string header)
-        {
-            IKTMHeader(s, Encoding.UTF8.GetBytes(header));
-        }
-        public static void IKTMHeader(Stream s, byte[] header)
-        {
-            if (header.Length == 4)
+            /// <summary>
+            /// Adds "BM" header to the file.
+            /// </summary>
+            /// <param name="s"></param>
+            public static void BMHeader(Stream s)
             {
-                s.Write(header);
+                s.Write(IDField);
             }
-            else
+            /// <summary>
+            /// Adds BMP size header to the file.
+            /// </summary>
+            /// <param name="s"></param>
+            /// <param name="BMPSize"></param>
+            public static void FileSizeHeader(Stream s, int BMPSize)
             {
-                throw new ArgumentException("Header length should be 4 bytes.");
+                byte[] Bytes = BitConverter.GetBytes(BMPSize);
+                s.Write(Bytes);
             }
-        }
-        /// <summary>
-        /// Writes offset to pixel array to the file.
-        /// </summary>
-        /// <param name="s"></param>
-        public static void PixelOffsetHeader(Stream s)
-        {
-            s.Write(DataOffset);
-        }
-        /// <summary>
-        /// Manages DIB header creation
-        /// </summary>
-        /// <param name="s"></param>
-        /// <param name="w"></param>
-        /// <param name="h"></param>
-        public static void DIB(Stream s, int w, int h)
-        {
-            s.Write(DIBNumber);
-            byte[] WBytes = BitConverter.GetBytes(w);
-            byte[] HBytes = BitConverter.GetBytes(h);
-            s.Write(WBytes);
-            s.Write(HBytes);
-            s.Write(DIB_MagicBytes_1);
-            int padding = w % 4;
-            byte[] RawBitmapSize = BitConverter.GetBytes((w * 3 + padding) * h);
-            s.Write(RawBitmapSize);
-            s.Write(DIB_MagicBytes_2);
+            /// <summary>
+            /// Adds program-specific unused value to the file.
+            /// </summary>
+            /// <param name="s"></param>
+            /// <param name="header"></param>
+            public static void IKTMHeader(Stream s, string header)
+            {
+                IKTMHeader(s, Encoding.UTF8.GetBytes(header));
+            }
+            public static void IKTMHeader(Stream s, byte[] header)
+            {
+                if (header.Length == 4)
+                {
+                    s.Write(header);
+                }
+                else
+                {
+                    throw new ArgumentException("Header length should be 4 bytes.");
+                }
+            }
+            /// <summary>
+            /// Writes offset to pixel array to the file.
+            /// </summary>
+            /// <param name="s"></param>
+            public static void PixelOffsetHeader(Stream s)
+            {
+                s.Write(DataOffset);
+            }
+            /// <summary>
+            /// Manages DIB header creation
+            /// </summary>
+            /// <param name="s"></param>
+            /// <param name="w"></param>
+            /// <param name="h"></param>
+            public static void DIB(Stream s, int w, int h)
+            {
+                s.Write(DIBNumber);
+                byte[] WBytes = BitConverter.GetBytes(w);
+                byte[] HBytes = BitConverter.GetBytes(h);
+                s.Write(WBytes);
+                s.Write(HBytes);
+                s.Write(DIB_MagicBytes_1);
+                int padding = w % 4;
+                byte[] RawBitmapSize = BitConverter.GetBytes((w * 3 + padding) * h);
+                s.Write(RawBitmapSize);
+                s.Write(DIB_MagicBytes_2);
+            }
         }
     }
 }
